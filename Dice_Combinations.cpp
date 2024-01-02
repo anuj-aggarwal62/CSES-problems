8c87944e689f2e6c0a1334fa1ff42a23707ab64c
Anuj Aggarwal <aggarwal.anuj62@gmail.com>
2024-01-03 02:16:32 +0530

    DP first

---
+++ Dice_Combinations.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define pi pair<int,int>
+#define pii pair<int,pair<int,int>>
+#define all(x) x.begin(), x.end()
+#define Sort(x) sort(all(x))
+#define display(v) for(auto i:v)cout<<i<<" ";
+#define vi vector<int>
+#define vl vector<long long>
+#define vb vector<bool>
+#define vvi vector<vector<int>>
+#define vvl vector<vector<long long>>
+#define vvb vector<vector<bool>>
+#define sti set<int>
+#define stl set<long long>
+#define mpii map<int,int>
+#define mpll map<long long,long long>
+#define yes cout<<"YES"<<endl
+#define no cout<<"NO"<<endl
+#define loop(i, N) for (int i = 0; i < N; i++)
+#define rep(i,x,n) for(int i=x;i<=n;i++)
+#define rev(i,n,x) for(int i=n;i>=x;i--)
+#define mod 1000000007
+void input(vi &v) {for(int i=0;i<(int)v.size();i++) cin>>v[i];}
+
+signed main() {
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+
+    int n;
+    cin>>n;
+
+    vi dp(n+1,0);
+    
+    for(int i=1;i<=min((int)6,n);i++) dp[i]++;
+
+    for(int i=2;i<=n;i++)
+    {
+        for(int j=1;j<=6;j++)
+        {
+            if(i-j>0)
+            {
+                dp[i] += dp[i-j];
+                dp[i] %= mod;
+            }
+        }
+    }
+
+    cout<<dp[n]<<endl;
+    
+
+    
+}
